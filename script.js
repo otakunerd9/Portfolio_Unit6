3e1e64fe84060dec269f14f98229732f5421d8e4
Andre I <aisaac2020@bklawtech.com>
2017-01-17 20:49:25 +0000

    star eyes

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 $(document).ready(function(){
     //Write all your jQuery code here
         $("button").click(function() {
-      $("#like").html("You like this project!");
+      $(".like").html("You like this project!");
     });
     
-    
-    
+     $(function() {
+        $(".nav").hover(
+            function() {
+                $(this).attr("src", "https://i.makeagif.com/media/11-19-2015/JcG1dE.gif");
+            },
+            function() {
+                $(this).attr("backgorund", "transparent");
+            }                         
+        );                  
+    });
     
 });
+
